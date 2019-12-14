3fb4e840b1f20b2608999106ae61bbea7e7172be
M Farkas-Dyck <strake888@gmail.com>
2019-12-14 01:25:32 -0800

    string

--- src/Text/Regex/Applicative/Lex.hs
+++ src/Text/Regex/Applicative/Lex.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE RecordWildCards #-}
-module Text.Regex.Applicative.Lex (natural', natural, ident', ident, IdentSpec (..), defaultIdentSpec) where
+module Text.Regex.Applicative.Lex (natural', natural, ident', ident, IdentSpec (..), defaultIdentSpec, string, esc, unicodepoint) where
 
-import           Control.Monad ((>=>), guard)
+import           Control.Applicative.Combinators (between, count')
+import           Control.Monad ((>=>), guard, replicateM)
 import qualified Data.Char.Properties.DerivedCore as UC
 import           Data.Foldable
 import           Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Normalize as T
 import           Numeric.Natural
-import           Text.Regex.Applicative
+import           Text.Regex.Applicative hiding (string)
+import qualified Text.Regex.Applicative as RE
 import           Util hiding (some)
 
 natural' :: RE Char Natural
@@ -22,7 +25,16 @@ natural' = asum
   ]
 
 natural :: Word -> RE Char Natural
-natural r = foldl' (\ n w -> fromIntegral r*n + fromIntegral w) 0 <$> some (msym (digit >=> \ n -> n <$ guard (n < r)))
+natural = naturalWith some
+
+naturalWith :: (RE Char Word -> RE Char [Word]) -> Word -> RE Char Natural
+naturalWith f r = fromDigits r <$> f (digit' r)
+
+fromDigits :: Integral a => Word -> [a] -> Natural
+fromDigits r = foldl' (\ n w -> fromIntegral r*n + fromIntegral w) 0
+
+digit' :: Word -> RE Char Word
+digit' r = msym (digit >=> \ n -> n <$ guard (n < r))
 
 ident' :: RE Char Text
 ident' = ident defaultIdentSpec
@@ -56,3 +68,28 @@ defaultIdentSpec = IdentSpec
         '\x2027' -> '·'
         '\x30FB' -> '·'
         x       -> x
+
+string :: RE Char TL.Text
+string = TL.pack <$> between (sym '"') (sym '"') (many xre)
+  where xre = psym (∉ "\"\\") <|> sym '\\' *> (psym (∈ "\"\\") <|> esc)
+
+esc :: RE Char Char
+esc = asum
+    [toEnum . fromIntegral <$ sym 'x' <*> naturalWith (replicateM 2) 16
+    , sym 'u' *> between (sym '{') (sym '}') unicodepoint
+    , '\x00' <$ sym '0'
+    , '\x07' <$ sym 'a'
+    , '\x08' <$ sym 'b'
+    , '\x09' <$ sym 't'
+    , '\x0A' <$ sym 'n'
+    , '\x0B' <$ sym 'v'
+    , '\x0C' <$ sym 'f'
+    , '\x0D' <$ sym 'r'
+    , '\x1B' <$ sym 'e'
+    ]
+
+unicodepoint :: RE Char Char
+unicodepoint = (\ a b -> toEnum . fromIntegral $ 0x10000 * a + b)
+    <$> asum [16 <$ RE.string "10", naturalWith (fmap pure) 16]
+    <*> naturalWith (replicateM 4) 16
+    <|> toEnum . fromIntegral <$> naturalWith (count' 0 4) 16
